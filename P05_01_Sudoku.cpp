0ea8ac7477744e8a6c8e56cc7d51fb3b8e62dc32
Luke <luke.vincent.braun@gmail.com>
2021-11-05 10:59:02 +0100

    fixing terminal output for P05_01

--- P05_01_Sudoku.cpp
+++ P05_01_Sudoku.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
     //variables
-    int zh = 0;
+    int zh = 0, linum = 9, numcompare = 9;
     int vartmp[5];
 
    for (zh = 0; zh < 5; zh++) {
@@ -14,10 +14,39 @@ int main() {
            cin >> vartmp[zh];
        } while (vartmp[zh] > 9 || vartmp[zh] < 1);
    }
+    
+    // print lines
+    for ( int y = 0; y < 9; y++) {
 
-    for (int i = 0; i < 5; i++)
-        cout << vartmp[i] << endl;
+        // print rows
+        for (int x = 0; x < 6; x++) {
 
-    return 0;
+            //print line number if you come to first line
+            if (x == 0) {
+                cout << linum;
+                linum--;
+            } else {
+                cout << ".";
+            }
+
+            // for (int numcheck = 0; numcheck < 5; numcheck++ ) {
+            //     if (vartmp[numcheck] == numcompare - x) {
+            //         cout << "+";
+            //     } else {
+            //         cout << ".";
+            //     }
+            // }
+    
+        } // end rows
+        
+        // break line after each row
+        cout << "\n";
+
+        // print last line with x-axis for graph legend
+        if (y == 8) {
+            cout << " 12345" << endl;
+        }
+
+    } // end lines
 
 }
