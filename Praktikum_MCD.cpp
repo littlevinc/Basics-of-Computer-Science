c0f36604fc09f4c8ffb804f3f204e1ea6be733cf
Raoul Duke <luke.vincent.braun@gmail.com>
2021-11-07 17:51:29 +0100

    Update Praktikum_MCD.cpp

--- Praktikum_MCD.cpp
+++ Praktikum_MCD.cpp
@@ -2,9 +2,30 @@
 #include <string>
 using namespace std;
 
+
+string encrypt(string text, int s)
+{
+	string result = "";
+
+	for (int i = 0; i < text.length(); i++)
+	{
+		if (isupper(text[i]))
+			result += char(int(text[i] + s - 65) % 26 + 65);
+
+		else if (islower(text[i]))
+			result += char(int(text[i] + s - 97) % 26 + 97);
+
+		else
+			result += text[i];
+	}
+
+	return result;
+}
+
+
 int main() {
 
-    string txtdecry = "OTP NLPDLC - GPCDNSWFPDDPWFYR(LFNS LWD NLPDLC - NSTQQCP, "
+    string txtdecry =   "OTP NLPDLC - GPCDNSWFPDDPWFYR(LFNS LWD NLPDLC - NSTQQCP, "
                         "NLPDLC - LWRZCTESXFD, NLPDLC - GPCDNSTPMFYR, GPCDNSTPMPNSTQQCP "
                         "ZOPC LWD PTYQLNSPC NLPDLC MPKPTNSYPE) TDE PTY PTYQLNSPD DJXXPECTDNSPD "
                         "GPCDNSWFPDDPWFYRDGPCQLSCPY, OLD LFQ OPC XZYZRCLASTDNSPY FYO "
@@ -19,8 +40,8 @@ int main() {
 
     int count[27] = { 0 };
 
+    // Analyse der Buchstaben
     for (char buch = 'A'; buch <= 'Z' ; buch++ ) {
-
         for (int i = 0; i < txtdecry.length(); i++ ) {
 
             if(txtdecry.at(i) == buch) {
@@ -30,9 +51,14 @@ int main() {
 
     }
 
+    //encrypt(txtdecry, 26-3)
+
+    cout << *std::max_element(array, array + n) << "\n";
+
+
+
+
     char ch = 'A';
-
-
     for(int x = 0; x < 26; x++) {
         cout << ch;
         cout << count[x] << endl;
