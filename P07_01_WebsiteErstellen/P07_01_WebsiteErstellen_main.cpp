bc4b41f07d0ff81e69f7fd587ca08b22b5593c36
Luke <luke.vincent.braun@gmail.com>
2021-11-23 15:27:43 +0100

    Fuck this bullshit

--- P07_01_WebsiteErstellen/P07_01_WebsiteErstellen_main.cpp
+++ P07_01_WebsiteErstellen/P07_01_WebsiteErstellen_main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+
+// globally defined struct
+struct personalinfo {
+		std::string vorname, name, birthdate;
+	};
+
+void splitWords(std::ifstream,  personalinfo[]);
 
 int main() {
+
+
+
+	// creat array of personalinfo struct
+	personalinfo test1[5];
 
 	std::string html = "";
 	std::string personen = "";
 
+	
+	// open both files into variable
 	std::ifstream htmlseite("webseite.html.tmpl");
 	std::ifstream personendaten("personendaten.txt");
 
@@ -22,9 +37,24 @@ int main() {
 				std::cout << html << std::endl;
 			}
 		}
-
-
-	
 	}
 
+	splitWords(personendaten, test1);
+
+
+}
+
+void splitWords(std::ifstream& datei, personalinfo info[]) {
+
+       std::string htmlline = "";
+
+       while(std::getline(datei, htmlline)) {
+
+              std::istringstream iss(htmlline);
+              std::string word;
+              while(iss >> word) {  
+                     std::cout << word << std::endl;
+              }
+       } 
+    
 }
---
+++ P07_01_WebsiteErstellen/conversion.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+void filetostruct(struct personen;) {
+
+}
