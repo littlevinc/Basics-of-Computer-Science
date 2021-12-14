df58b6455c9e6176979c614e28e76a7d37e8521c
Raoul Duke <luke.vincent.braun@gmail.com>
2021-12-14 11:16:57 +0100

    Aufgabe 3 Praktikum 10 Unit Testing

---
+++ P10_03_UnitTesting/MyRectangle.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class MyRectangle {
+
+private:
+
+	int x1 = 0, y1 = 0; // upper left corner
+	int x2 = 20, y2 = 20; // lower right corner
+
+public:
+
+	// constructor
+	MyRectangle(int tmp1, int tmp2, int tmp3, int tmp4);
+
+	// setter methods
+	void set(int tmp1, int tmp2, int tmp3, int tmp4);
+	void setX1(int tmp);
+	void setY1(int tmp);
+	void setX2(int tmp);
+	void setY2(int tmp);
+
+	// getter mehtods
+	int getX1();
+	int getY1();
+	int getX2();
+	int getY2();
+
+	void draw() const;
+
+	bool does_not_collide_with(const MyRectangle& other) const;
+
+};
---
+++ P10_03_UnitTesting/main.cpp
@@ -0,0 +1,15 @@
+// Datei: main.cpp
+
+#include <iostream>
+using namespace std;
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.h"
+
+int main()
+{
+	Catch::Session().run();
+
+	system("PAUSE");
+	return 0;
+}
